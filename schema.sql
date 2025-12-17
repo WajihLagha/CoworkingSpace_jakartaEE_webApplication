ac4770365f2c4b20dae2cb8d64209cfe819721a4
Wajih Lagha <wajih.lagha@isimg.tn>
2025-12-17 21:56:44 +0100

    feat: Add initial database schema for coworking space management.

---
+++ schema.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 8.3.0, for Win64 (x86_64)
+--
+-- Host: localhost    Database: coworking_db
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+--
+-- Table structure for table `espacetravail`
+--
+
+DROP TABLE IF EXISTS `espacetravail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `espacetravail` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nom` varchar(80) NOT NULL,
+  `typeEspace` enum('BUREAU','SALLE_REUNION','SALLE_FORMATION') NOT NULL,
+  `capacite` int NOT NULL,
+  `prixHoraire` decimal(10,2) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservation` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `dateHeureDebut` datetime NOT NULL,
+  `dateHeureFin` datetime NOT NULL,
+  `statut` enum('EN_ATTENTE','CONFIRMEE','ANNULEE') NOT NULL,
+  `montantTotal` decimal(10,2) NOT NULL,
+  `dateCreation` datetime NOT NULL,
+  `utilisateur_id` bigint NOT NULL,
+  `espaceTravail_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `utilisateur_id` (`utilisateur_id`),
+  KEY `espaceTravail_id` (`espaceTravail_id`),
+  CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`),
+  CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`espaceTravail_id`) REFERENCES `espacetravail` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reservation_supplement`
+--
+
+DROP TABLE IF EXISTS `reservation_supplement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservation_supplement` (
+  `reservation_id` bigint NOT NULL,
+  `supplement_id` bigint NOT NULL,
+  PRIMARY KEY (`reservation_id`,`supplement_id`),
+  KEY `supplement_id` (`supplement_id`),
+  CONSTRAINT `reservation_supplement_ibfk_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `reservation_supplement_ibfk_2` FOREIGN KEY (`supplement_id`) REFERENCES `supplement` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `supplement`
+--
+
+DROP TABLE IF EXISTS `supplement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supplement` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(60) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `prixUnitaire` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utilisateur` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `motDePasse` varchar(100) NOT NULL,
+  `typeUtilisateur` enum('ETUDIANT','ENTREPRISE','PARTICULIER','ADMIN') NOT NULL,
+  `nomEntreprise` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-17 21:52:30
